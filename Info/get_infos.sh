6fda960d50bb02d377509c515898bd6fd3d65339
remilap <remi.lapointe@gmail.com>
2017-03-21 17:25:56 +0100

    Adding more information requests

--- Info/get_infos.sh
+++ Info/get_infos.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 infos_file=/tmp/infos.txt
 cat << EOF > ${infos_file}
@@ -7,11 +7,20 @@ cat << EOF > ${infos_file}
 2:measure_volts:sdram voltage:core sdram_c sdram_i sdram_p
 3:measure_temp:temperature of BCM2835 SoC:
 4:codec_enabled:enabled codec:H264 MPG2 WVC1 MPG4 MJPG WMV9
-5:get_config:Raspberry configuration:
+5:get_config:Raspberry configuration:int
 6:get_mem:CPU and GPU memory:arm gpu
 7:version:firmware version:
 8:otp_dump:contents of the OTP memory:
 9:get_lcd_info:display framebuffer:
+10:get_camera:camera information:
+11:hdmi_status_show:HDMI status:
+12:hdmi_ntsc_freqs:HDMI NTSC frequency
+13:pm_get_status:pm get status:
+14:mem_oom:Out Of Memory:
+15:/proc/cpuinfo:CPU information:
+16:/proc/version:OS version:
+17:/proc/meminfo:memory information:
+18:free:memory information summary:
 EOF
 
 min_opt=`head -n 1 ${infos_file} | cut -d: -f1`
@@ -44,12 +53,23 @@ get_info() {
   command=`head -n ${num_line} ${infos_file} | tail -n 1 | cut -d: -f2`
   args=`head -n ${num_line} ${infos_file} | tail -n 1 | cut -d: -f4`
   head -n ${num_line} ${infos_file} | tail -n 1 | cut -d: -f3
-  if [ "${args}" = "" ]; then
-    vcgencmd ${command}
+  cmde2=${command:0:5}
+  if [ "${cmde2}" = "/proc" ]; then
+    cat ${command}
+  elif [ "${command}" = "free" ]; then
+    free -o -h
   else
-    for arg in ${args}; do
-      echo "${arg}:\t$(vcgencmd ${command} ${arg})"
-    done
+    if [ "${args}" = "" ]; then
+      vcgencmd ${command}
+    else
+      if [ "${command}" = "get_config" ]; then
+        vcgencmd ${command} ${args}
+      else
+        for arg in ${args}; do
+          echo -e "${arg}:\t$(vcgencmd ${command} ${arg})"
+        done
+      fi
+    fi
   fi
   echo
 }
