29f6368319f73326b6fc02139d09314ce7608b59
remilap <remi.lapointe@gmail.com>
2017-05-08 22:36:14 +0200

    Add get_external_IP.py call in crontab and check with latest registered IP address

--- Info/get_external_IP.py
+++ Info/get_external_IP.py
@@ -7,5 +7,22 @@ import send_sms
 cmde="wget http://checkip.dyndns.org/ -O - -o /dev/null | cut -d: -f 2 | cut -d\< -f 1"
 sortie=os.popen(cmde, "r").read()
 
-send_sms.send("Adresse IP externe : " + sortie)
+fnIPaddr="/home/pi/latest_external_IP_addr.txt"
+
+try:
+    fm = open(fnIPaddr, 'r')
+    contenu = fm.read()
+    print('contenu du fichier ' + fnIPaddr + ' : ' + contenu)
+    fm.close()
+except:
+    contenu = 0
+    print('fichier ' + fnIPaddr + ' inexistant => contenu = 0')
+
+if sortie != contenu:
+    # write last IP addr in a file
+    fo = open(fnIPaddr, 'w')
+    fo.write(sortie + '\n')
+    fo.close()
+
+    send_sms.send("Nouvelle adresse IP externe : " + sortie)
 
