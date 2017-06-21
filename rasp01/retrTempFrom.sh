a4e5f41d02ea98b16ba6036e3033a6bcc9877391
remilap <remi.lapointe@gmail.com>
2017-06-21 07:55:29 +0200

    Add -f option in retrTempFrom.sh

--- rasp01/retrTempFrom.sh
+++ rasp01/retrTempFrom.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+force=0
+if [ "$1" = "-f" ]; then
+  force=1
+  shift
+fi
 client=rasp02
 if [ "$1" != "" ]; then
   client=$1
@@ -9,7 +14,7 @@ file=/home/pi/last_temp
 
 # Check if the client host is reachable
 ping -q -c 3 -i 1 ${client} > /dev/null 2>&1
-if [ $? = 0 ]; then
+if [ $? = 0 -o ${force} = 1 ]; then
   ssh ${client} "tail -n 1 temps.csv" > ${file}.tmp
   nl=`wc -l ${file}.tmp | awk '{print $1}'`
   if [ ${nl} -gt 0 ]; then
