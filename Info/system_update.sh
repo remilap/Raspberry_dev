3e3e800f76c4b278315964c3c030a5332f55c3ab
remilap <remi.lapointe@gmail.com>
2017-09-04 16:17:07 +0200

    Inverse upgrade and dist-upgrade

--- Info/system_update.sh
+++ Info/system_update.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+################################################################################
+# 
+#-------------------------------------------------------------------------------
+# HISTORY:
+# 2017-09-04 - R. Lapointe    - Inverse upgrade and dist-upgrade
+################################################################################
 
 sudo apt-get update
 
-sudo apt-get upgrade
-
 sudo apt-get dist-upgrade
+
+sudo apt-get upgrade
 
 sudo apt-get autoremove
 
