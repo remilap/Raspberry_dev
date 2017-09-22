07226eac89257ffd8e9a051c5e8e1a85354130c1
remilap <remi.lapointe@gmail.com>
2017-09-22 14:17:50 +0200

    Fix library directory issue in get_external_IP.py

--- Info/get_external_IP.py
+++ Info/get_external_IP.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python
 
 import os, sys
-sys.path.append(os.path.dirname(__file__) + "/../Lib")
+d = os.path.dirname(__file__)
+d = d if d else '.'
+sys.path.append(d + "/../Lib")
 import send_sms
 
 cmde="wget http://checkip.dyndns.org/ -O - -o /dev/null | cut -d: -f 2 | cut -d\< -f 1"
 sortie=os.popen(cmde, "r").read()
 
-fnIPaddr="/home/pi/latest_external_IP_addr.txt"
+fnIPaddr="$HOME/latest_external_IP_addr.txt"
 
 try:
     fm = open(fnIPaddr, 'r')
     contenu = fm.read()
-    print('contenu du fichier ' + fnIPaddr + ' : ' + contenu)
+#    print('contenu du fichier ' + fnIPaddr + ' : ' + contenu)
     fm.close()
 except:
     contenu = 0
-    print('fichier ' + fnIPaddr + ' inexistant => contenu = 0')
+#    print('fichier ' + fnIPaddr + ' inexistant => contenu = 0')
 
 if sortie != contenu:
     # write last IP addr in a file
