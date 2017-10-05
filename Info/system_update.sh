3fa00fc2ca831483f952733c36b722e8c5221856
remilap <remi.lapointe@gmail.com>
2017-10-05 15:10:22 +0200

    Remove purge-old-kernels command (not found)

--- Info/system_update.sh
+++ Info/system_update.sh
@@ -6,6 +6,7 @@
 # 2017-09-04 - R. Lapointe    - Inverse upgrade and dist-upgrade
 # 2017-10-04 - R. Lapointe    - Group all apt-get commands in a list and add purge-old-kernels
 # 2017-10-05 - R. Lapointe    - Add option -y for avoiding tasks to be blocked
+# 2017-10-05 - R. Lapointe    - Remove purge-old-kernels command (not found)
 ################################################################################
 
 cmd_list="update dist-upgrade upgrade autoremove clean"
@@ -13,6 +14,4 @@ for cmd in ${cmd_list}; do
   echo "===== sudo apt-get ${cmd}"
   sudo apt-get -y ${cmd}
 done
-
-sudo purge-old-kernels --keep 2
 
