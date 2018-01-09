173769f3eb1d570c90f7c184bd821f73926f41b5
remilap <remi.lapointe@gmail.com>
2018-01-09 16:57:52 +0100

    Add configparser install and use in Temp.py

--- Sensor/Temp.py
+++ Sensor/Temp.py
@@ -7,18 +7,38 @@ import add_value_initState
 import manage_low_temp
 from subprocess import check_output
 
+# retrieve the home directory
+from os.path import expanduser
+home_dir = expanduser("~")
+print 'home_dir=' + home_dir
+
+# init default corrective temperature
+t_cor = 6.8
+
+# read properties file
+import configparser
+config = configparser.RawConfigParser()
+config.read(home_dir + '/Temp.ini')
+try:
+    t_cor = float(config.get('TempSection', 'shift_derive'))
+except:
+    print "unable to find 'shift_derive'"
+
+print 't_cor=' + str(t_cor)
 
 t = get_temp_envirophat.read_temp()
 
 internal_temp = check_output("vcgencmd measure_temp | cut -d= -f2 | cut -d\\' -f1", shell=True)
 int_t = float(internal_temp.strip())
 cor_t = t - int_t / 6.1
+t -= t_cor
 print '{0:.1f} {1:.1f} {2:.1f}'.format(t, int_t, cor_t)
 
 if (len(sys.argv) > 1):
     # Send value to initialstate.com
     add_value_initState.send_value(t)
 
+    t = round(t*10)/10
     manage_low_temp.analyze(t)
 
 
