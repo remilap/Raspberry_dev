7f71013d5f9c4557dc71c070b9b96e587e50ebd6
remilap <remi.lapointe@gmail.com>
2021-08-24 09:56:02 +0200

    Update Raspbian to buster version

--- Info/system_update.sh
+++ Info/system_update.sh
@@ -7,11 +7,29 @@
 # 2017-10-04 - R. Lapointe    - Group all apt-get commands in a list and add purge-old-kernels
 # 2017-10-05 - R. Lapointe    - Add option -y for avoiding tasks to be blocked
 # 2017-10-05 - R. Lapointe    - Remove purge-old-kernels command (not found)
+# 2021-08-24 - R. Lapointe    - Replace apt-get by apt
+# 2021-08-24 - R. Lapointe    - Manage the OS upgrade to Debian buster
 ################################################################################
+
+sfile=/etc/apt/sources.list
+rel="buster"
+cur=$(grep "^deb.*raspbian" $sfile | awk '{print $3}')
+if [ "$cur" = "" ]; then
+  echo "===== ERROR: unable to find the current version of Debian"
+  echo "please see current content of $sfile"
+  cat $sfile
+  exit 1
+fi
+echo "===== Current Debian version is $cur"
+
+if [ "$cur" != "$rel" ]; then
+  echo "===== Upgrade Debian version from $cur to $rel"
+  find /etc/apt -type f -exec grep $cur {} \; -exec sudo sed -i s/$cur/$rel/ {} \; -print
+fi
 
 cmd_list="update dist-upgrade upgrade autoremove clean"
 for cmd in ${cmd_list}; do
-  echo "===== sudo apt-get ${cmd}"
-  sudo apt-get -y ${cmd}
+  echo "===== sudo apt ${cmd}"
+  sudo apt -y ${cmd}
 done
 
